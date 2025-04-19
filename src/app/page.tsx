108cb9b987ddd32f3efedd0323ff94835e60e663
giribalan27 <giribalan092@gmail.com>
2025-04-19 13:10:32 +0530

    customization component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Checkbox} from "@/components/ui/checkbox";
+import {Progress} from "@/components/ui/progress";
+import {Textarea} from "@/components/ui/textarea";
 
 const Page = () => {
 	return (
-		<div className={"font-bold text-4xl text-black underline"}>
-			Hello world
-		</div>
+		<main className={" p-10 flex flex-col gap-7"}>
+			<Button variant={"elevated"} >
+				Hello
+			</Button>
+			<Input placeholder={"Enter"}/>
+			<Checkbox />
+			<Progress value={50}/>
+			<Textarea placeholder={"Enter"}/>
+		</main>
 	)
 }
 export default Page
